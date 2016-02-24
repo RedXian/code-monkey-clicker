b8a8f9774745cb0c8c027c9e2e16e4fd86d79e2a
Christian McNeill <me@redxian.com>
2016-02-24 11:14:36 +1000

    Changes

---
+++ WebApplication1/WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebApplication1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Melbourne IT")> 
+<Assembly: AssemblyProduct("WebApplication1")> 
+<Assembly: AssemblyCopyright("Copyright © Melbourne IT 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("272a2096-faf9-4ac1-8637-d51175205a78")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
